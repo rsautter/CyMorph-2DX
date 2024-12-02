befcad94dd21aed1911356a5ce48f52ea97d587e
Rubens <rubens.sautter@gmail.com>
2024-12-02 07:35:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,12 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-import os
+from setuptools import find_packages, setup
 
-print("Numpy Location:"+numpy.get_include())
-setup(ext_modules=cythonize(["*.pyx"], annotate=True,
-		compiler_directives={
-			'optimize.use_switch': True,
-			'initializedcheck':False,
-			'overflowcheck':False,
-			'optimize.unpack_method_calls':True,
-			'boundscheck':False,
-			'profile': False,
-			'infer_types':True,
-			'cdivision_warnings': False,
-			'cdivision': True,
-			'wraparound': False,
-			'boundscheck': False},
-                include_path=[numpy.get_include()]
-),
-include_dirs=[numpy.get_include()]
+setup(
+    name='CyMorph2',
+    version='2.0',
+    url='https://github.com/rsautter/CyMorph-2DX',
+    author='Rubens Andreas Sautter',
+    author_email='rubens.sautter@gmail.com',
+    keywords='Galaxy Morphology Entropy GPA',
+    description=u'A non-parametric morphological package',
+    packages=find_packages()
 )
